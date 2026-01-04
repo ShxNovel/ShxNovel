09d324eb4f693a5a6b26524b1259f7c7f7d42b8b
aurora5090 <aurora5090@qq.com>
2026-01-05 00:15:48 +0800

    [init] init monorepo

---
+++ packages/canoe/vite.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    test: {
+        root: 'test',
+        clearMocks: true,
+        typecheck: {
+            enabled: true,
+        },
+    },
+    build: {
+        lib: {
+            entry: './src/index.ts',
+            name: 'canoe',
+            fileName: 'canoe',
+            formats: ['es'],
+        },
+        rollupOptions: {
+            external: ['gsap', 'three', 'stats.js'],
+            output: {
+                inlineDynamicImports: false,
+                preserveModules: true,
+                entryFileNames: ({ name: fileName }) => {
+                    return `${fileName}.js`;
+                },
+                globals: {
+                    gsap: 'gsap',
+                    three: 'THREE',
+                    stats: 'Stats',
+                },
+            },
+        },
+    },
+});
